879177f9af9b3f71e04f01475a54a1160c216cb8
Freddy Perez <fred.perez15@gmail.com>
2023-05-31 16:14:33 -0400

    added styling, working on functionality

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Button, styled } from "@mui/material";
+import Logout from "./Logout";
+import Login from "./Login";
 
 const StyledLink = styled(Link)({
   color: "white",
@@ -8,22 +10,22 @@ const StyledLink = styled(Link)({
 });
 
 function Navbar() {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My App
-        </Typography>
-        <nav>
-          <StyledLink to="/ingredients">Add Ingredients</StyledLink>
-          <StyledLink to="/recipes">Add Recipes</StyledLink>
-          <StyledLink to="/recipe_menu">My Menu</StyledLink>
-          <StyledLink to="/home">Home</StyledLink>
-        </nav>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
-  );
-}
+    return (
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Bar Par
+            </Typography>
+            <nav>
+              <StyledLink to="/ingredients">Add Ingredients</StyledLink>
+              <StyledLink to="/recipes">Add Recipes</StyledLink>
+              <StyledLink to="/recipe_menu">My Menu</StyledLink>
+              <StyledLink to="/home">Home</StyledLink>
+            </nav>
+          </Toolbar>
+        </AppBar>
+      );
+    }
+    
 
 export default Navbar;
